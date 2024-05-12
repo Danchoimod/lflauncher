402a51911bca7518f2ac412897b155d4a69fc2cb
danchoimod <tranphupham2909@gmail.com>
2024-05-12 17:42:43 +0700

    rickroll

--- assets/js/js.js
+++ assets/js/js.js
@@ -139,7 +139,16 @@ $(document).ready(function(){
   })
 
   /* BackTop */
-
+  $(window).scroll(function() {
+    if ($(this).scrollTop()) {
+      $('#backtop').fadeIn();
+    } else {
+      $('#backtop').fadeOut();
+    }
+  });
+  $('#backtop').click(function() {
+    $('html, body').animate({scrollTop: 0}, 800);
+  });
 
 
 });
