0868be718957214e23500f762fc4a93e712e7f0f
Danchoimod <113335341+Danchoimod@users.noreply.github.com>
2025-02-14 18:45:18 +0700

    Update js.js

--- assets/js/js.js
+++ assets/js/js.js
@@ -1,5 +1,6 @@
 /* Scroll */
-
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8177702634836557"
+     crossorigin="anonymous"></script>
 window.onscroll = function() {showProduct()};
 
 function showProduct() {
